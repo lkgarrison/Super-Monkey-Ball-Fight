8e318fd242accfb8b7049bb83258c5c36cac2beb
Luke Garrison <lgarriso@nd.edu>
2016-04-30 19:02:47 -0400

    Updates p1's position based on received keypress
    Also sends back the updated gamestate which reflects the new position of
    p1

--- server.py
+++ server.py
@@ -13,98 +13,110 @@ P1_PORT = 42201
 P2_PORT = 42202
 
 class Player1CommandConnection(Protocol):
-    def __init__(self, addr, gameServer):
-        self.addr = addr
-        self.gameServer = gameServer
-        self.gameServer.p1_connection = self
-        print "connection received from", addr
-        print "p1 is connected"
+	def __init__(self, addr, gameServer):
+		self.addr = addr
+		self.gameServer = gameServer
+		self.gameServer.p1_connection = self
+		print "connection received from", addr
+		print "p1 is connected"
 
-    def connectionMade(self):
-        self.gameServer.p1_isConnected = True
-        if self.gameServer.isReadyToStart():
-            self.gameServer.sendStartSignal()
+	def connectionMade(self):
+		self.gameServer.p1_isConnected = True
+		if self.gameServer.isReadyToStart():
+			self.gameServer.sendStartSignal()
 
-    def dataReceived(self, data):
-        print "command received from p1"
-        print data
+	def dataReceived(self, data):
+		print "command received from p1"
+		print data
 
-        #process data
-        self.gameServer.sendGameState()
+		#process data
+		try:
+			key = int(data)
+			self.gameServer.gameState.p1_data.handleKeypress(key)
+		except:
+			pass
 
-    def connectionLost(self, reason):
-        print "connection lost from", self.addr
+		self.gameServer.sendGameState()
+
+	def connectionLost(self, reason):
+		print "connection lost from", self.addr
 
 class Player1CommandConnectionFactory(Factory):
-    def __init__(self, gameServer):
-        self.gameServer = gameServer
+	def __init__(self, gameServer):
+		self.gameServer = gameServer
 
-    def buildProtocol(self, addr):
-        return Player1CommandConnection(addr, self.gameServer)
+	def buildProtocol(self, addr):
+		return Player1CommandConnection(addr, self.gameServer)
 
 
 class Player2CommandConnection(Protocol):
-    def __init__(self, addr, gameServer):
-        self.addr = addr
-        self.gameServer = gameServer
-        print "connection received from", addr
-        print "p2 is connected"
-        self.gameServer.p2_connection = self
+	def __init__(self, addr, gameServer):
+		self.addr = addr
+		self.gameServer = gameServer
+		print "connection received from", addr
+		print "p2 is connected"
+		self.gameServer.p2_connection = self
 
-    def connectionMade(self):
-        self.gameServer.p2_isConnected = True
-        if self.gameServer.isReadyToStart():
-            self.gameServer.sendStartSignal()
+	def connectionMade(self):
+		self.gameServer.p2_isConnected = True
+		if self.gameServer.isReadyToStart():
+			self.gameServer.sendStartSignal()
 
-    def dataReceived(self, data):
-        print "command received from p2"
-        print data
 
-        #process data
-        self.gameServer.sendGameState()
+	def dataReceived(self, data):
+		print "command received from p2"
+		print data
 
-    def connectionLost(self, reason):
-        print "connection lost from", self.addr
+		#process data
+		self.gameServer.sendGameState()
+
+	def connectionLost(self, reason):
+		print "connection lost from", self.addr
 
 
 class Player2CommandConnectionFactory(Factory):
-    def __init__(self, gameServer):
-        self.gameServer = gameServer
+	def __init__(self, gameServer):
+		self.gameServer = gameServer
 
-    def buildProtocol(self, addr):
-        return Player2CommandConnection(addr, self.gameServer)
+	def buildProtocol(self, addr):
+		return Player2CommandConnection(addr, self.gameServer)
 
 
 class GameServer():
-    def __init__(self):
-        reactor.listenTCP(P1_PORT, Player1CommandConnectionFactory(self))
-        reactor.listenTCP(P2_PORT, Player2CommandConnectionFactory(self))
+	def __init__(self):
+		reactor.listenTCP(P1_PORT, Player1CommandConnectionFactory(self))
+		reactor.listenTCP(P2_PORT, Player2CommandConnectionFactory(self))
 
-        self.p1_isConnected = False;
-        self.p2_isConnected = False;
+		self.p1_isConnected = False;
+		self.p2_isConnected = False;
 
-        self.gameState = GameState()
+		self.gameState = GameState()
 
-    # returns True/False if both players are connected to the game server
-    def isReadyToStart(self):
-        if self.p1_isConnected and self.p2_isConnected:
-            return True
-        else:
-            return False
+	# returns True/False if both players are connected to the game server
+	def isReadyToStart(self):
+		if self.p1_isConnected and self.p2_isConnected:
+			return True
+		else:
+			return False
 
-    def sendStartSignal(self):
-        self.p1_connection.transport.write("start")
-        self.p2_connection.transport.write("start")
+	def sendStartSignal(self):
+		self.p1_connection.transport.write("start")
+		self.p2_connection.transport.write("start")
 
-    # send game state to all connected players
-    def sendGameState(self):
-        gameStateString = pickle.dumps(self.gameState)
-        if self.p1_connection is not None:
-            self.p1_connection.transport.write(gameStateString)
-        if self.p2_connection is not None:
-            self.p2_connection.transport.write(gameStateString)
+	# send game state to all connected players
+	def sendGameState(self):
+		gameStateString = pickle.dumps(self.gameState)
+		try:
+			self.p1_connection.transport.write(gameStateString)
+ 		except:
+ 			pass
+
+ 		try:
+ 			self.p2_connection.transport.write(gameStateString)
+ 		except:
+ 			pass
 
 if __name__ == "__main__":
-    sever = GameServer()
-    reactor.run()
+	sever = GameServer()
+	reactor.run()
 
